1439c721f3fc86bc8c3bc44f8ee720a23081d09b
Victoria Probst <victoria.probst13@gmail.com>
2022-05-24 14:18:06 -0400

    Generated Postman Framework and started GET Function

--- products/views.py
+++ products/views.py
@@ -1,7 +1,14 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import ProductSerializer
+from .models import Product
+from products import serializers 
+
 
 @api_view(['GET'])
 def products_list(request):
-
-    return Response('ok')
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    
+    
+    return Response(serializer.data)

6298200f4f294046fdc02778c50c6c829a2d6b5d
Victoria Probst <victoria.probst13@gmail.com>
2022-05-24 12:03:33 -0400

    Created Products Model for Products APP plus registered Products and REST Framework in Project Settings installed APPS section

--- products/models.py
+++ products/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=225)
+    description = models.CharField(max_length=225)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    inventory_quantity = models.IntegerField()
